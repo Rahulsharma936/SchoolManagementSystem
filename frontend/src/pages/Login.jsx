e8336250f79a3ae47dfff4a19ef4fd848431a976
Rahulsharma936 <rahulsharmawnl239@gmail.com>
2025-12-16 13:32:10 +0530

    third commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
             };
 
             const { data } = await axios.post(
-                '/api/users/login',
+                'https://school-management-system-5ci5.vercel.app/api/users/login',
                 { email, password },
                 config
             );
