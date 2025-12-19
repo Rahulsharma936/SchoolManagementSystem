555a65d0ac0ca11e23381f8cd541d96caa6d97b4
Rahulsharma936 <rahulsharmawnl239@gmail.com>
2025-12-19 17:02:12 +0530

    fifth commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+    API_URL: "https://schoolmanagementsystem-sjla.onrender.com"
 };
 
 export default config;
